2a2f1592b450ac39905874eba620c778bb917e45
you0104 <98068494+you0104@users.noreply.github.com>
2022-10-21 15:04:25 +0900

    数値リテラル

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
-const greeting = "Hello,";
-const target = "world!";
-console.log(greeting + target);
+const width1 = 5;
+const width2 = 8;
+const height = 3;
+const area = ((width1 + width2) * height) / 2;
+console.log(area);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-const greeting: string = "Hello,";
-const target: string = "world!";
-console.log(greeting + target);
+const width1 = 5;
+const width2 = 8;
+const height: number = 3;
+const area = ((width1 + width2) * height) / 2;
+
+console.log(area);
